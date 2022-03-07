60f13f2e0cf8a1012bed3a3e91521cd5f9b193ca
slavekrouta <97255074+slavekrouta@users.noreply.github.com>
2022-03-07 11:34:35 +0100

    O jedno heslo více

--- myscript.js
+++ myscript.js
@@ -2,6 +2,11 @@ function ladade()
 {
   if(document.getElementById("pass").value = "ᨫ")
   {
-    document.write("<a href=\"wld98hmj29cnxqrzieph.html\">Click here</a>");
+    document.write("<a href=\"wld98hmj29cnxqrziephpersonal.html\">Click here</a>");
+
+  }
+  else if(document.getElementById("pass").value = "bagr")
+  {
+    document.write("<a href=\"wld98hmj29cnxqrziephbagr.html\">Click here</a>");
   }
 }

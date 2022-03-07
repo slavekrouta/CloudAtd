d404c915e320ff0b7974f9a70f5be9a70e580d1e
slavekrouta <97255074+slavekrouta@users.noreply.github.com>
2022-03-07 11:30:05 +0100

    I changed password

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 function ladade()
 {
-  if(document.getElementById("pass").value = "slavkovohtml")
+  if(document.getElementById("pass").value = "ᨫ")
   {
     document.write("<a href=\"wld98hmj29cnxqrzieph.html\">Click here</a>");
   }

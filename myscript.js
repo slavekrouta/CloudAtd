627f1cde8aaf01abc5466c0144f8d89d2eb24d07
slavekrouta <97255074+slavekrouta@users.noreply.github.com>
2022-06-27 20:37:01 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -11,6 +11,6 @@ function ladade()
   }
   else if(document.getElementById("pass").value === "faktura")
   {
-    Document.write("image_2022-06-22_163713993.png");
+    document.write("<a href=\"image_2022-06-22_163713993.png\" download=\"Faktura.png\">Stáhnout fakturu ze sunnysoftu<a/>");
   }
 }
